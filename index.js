cd6d2f718ef73a735eddcc0323fb3d08dd546ed8
Saksham <sakshamchauhan79@gmail.com>
2022-11-22 10:56:18 +0530

    feat:add loger

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 
 const app = express();
 
+const logger = [];
+
 app.get("/", (req, res) => {
   console.log(req);
   res.status(200).send("running perfectly");
@@ -9,9 +11,17 @@ app.get("/", (req, res) => {
 
 app.post("/submit", (req, res) => {
   console.log(req.body);
+  logger.pop(req.body);
   res.status(201).send(true);
+});
+
+app.get("/check", (req, res) => {
+  res.status(200).send(logger);
 });
 
 app.listen(4040, () => {
   console.log("listening on port 4040");
 });
+
+// https://script.google.com/macros/s/AKfycbyV-_HqWzraogVF6DN-1gQ81pjOhwiQEWRaZ2sZ3B3VViqSamgBPTcDAbn21vifmr6w/exec
+// AKfycbyV-_HqWzraogVF6DN-1gQ81pjOhwiQEWRaZ2sZ3B3VViqSamgBPTcDAbn21vifmr6w
