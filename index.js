c4584274e04a9425beeeef5bb251439166273b1a
Saksham <sakshamchauhan79@gmail.com>
2022-11-22 10:24:42 +0530

    feat:add submit endpoint

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
   res.status(200).send("running perfectly");
 });
 
+app.post("/submit", (req, res) => {
+  console.log(req.body);
+  res.status(201).send(true);
+});
+
 app.listen(4040, () => {
   console.log("listening on port 4040");
 });
