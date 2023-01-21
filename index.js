e47fc83201d43b8be778fff248d2e2e96ffff7dd
Saksham <saksham@Sakshams-MacBook-Air.local>
2023-01-21 10:21:55 +0530

    feat:add distance endpoint

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
+app.options("*", cors());
 
 let logger = [];
 
 app.get("/", (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
   console.log(req);
   res.status(200).send("running perfectly");
+});
+
+app.get("/distance", async (req, res) => {
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  const distance = await getDistance(req.headers?.url);
+  res.status(200).json(distance);
 });
 
 app.post("/submit", (req, res) => {
@@ -27,6 +39,18 @@ app.get("/check", (req, res) => {
 app.listen(4040, () => {
   console.log("listening on port 4040");
 });
+
+// ------helpers------
+
+const getDistance = async (url) => {
+  try {
+    const res = await axios.get(url);
+    console.log(res, res?.data);
+    return res.data;
+  } catch (error) {
+    return false;
+  }
+};
 
 // https://script.google.com/macros/s/AKfycbyV-_HqWzraogVF6DN-1gQ81pjOhwiQEWRaZ2sZ3B3VViqSamgBPTcDAbn21vifmr6w/exec
 // AKfycbyV-_HqWzraogVF6DN-1gQ81pjOhwiQEWRaZ2sZ3B3VViqSamgBPTcDAbn21vifmr6w
